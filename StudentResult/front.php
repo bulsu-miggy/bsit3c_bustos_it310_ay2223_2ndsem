60879777fc6c47a4ee0d7c4a679964ef1e626a35
JAMV13 <joshuaangelo.victoria.m@bulsu.edu.ph>
2023-03-31 20:51:11 +0800

    File of StudentResult

---
+++ StudentResult/front.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="frontstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home Page</title>
+</head>
+<body style="background-color:#05445E;">
+   <header>
+       <nav>
+        <div style="background-color:#189AB4; border-radius:180px; ">
+           <img src="logop.png" class="logob">
+</div>
+<div style=" ">
+           <ul>
+               <li><a href="" style="color:white; background-color:#189AB4; padding:20px; border-radius:10px; ">HOME</a></li>
+               <li><a href="#about" style="color:white; background-color:#189AB4; padding:20px; border-radius:10px; ">ABOUT</a></li>
+               <li><a href="#contact" style="color:white; background-color:#189AB4;padding:20px; border-radius:10px; ">CONTACT</a></li>
+               <li><a href="index.php" style="color:white; background-color:#189AB4; padding:20px; border-radius:10px;">LOGIN</a></li>
+           </ul>
+</div>
+       </nav>
+
+       <div class="intro">
+        </div>
+        <section id="about">
+        <h4 style="color:white;">ABOUT</h4>
+        <br><br><br>
+        <br><br><br><br>
+        <center><p class="about" style="color:white;">&nbsp;&nbsp;This system is designed to provide the final grade result to the students in a simple way. This project 
+                is useful for students and institutions for getting the results in a simple manner.</p></center>
+                </section>
+    <footer>
+        <div class="footer-content">
+            <h3>Contact Us
+</h3>
+<section id="contact"><br><br>
+<a href=""><i class="fa fa-google">&nbsp;&nbsp;adrian.cruz.a@bulsu.edu.ph</i></a><br>
+<a href=""><i class="fa fa-google">&nbsp;&nbsp;jaztine.canoza.r@bulsu.edu.ph</i></a><br>
+<a href=""><i class="fa fa-google">&nbsp;&nbsp;jacob.cruz.a@bulsu.edu.ph</i></a><br>
+<a href=""><i class="fa fa-google">&nbsp;&nbsp;joshuaangelo.victoria.a@bulsu.edu.ph</i></a><br><br><br><br>
+</section>
+                <ul class="socials">
+                    <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                    <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href=""><i class="fa fa-youtube"></i></a></li>
+                    <li><a href=""><i class="fa fa-linkedin-square"></i></a></li>
+                </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>"© 2022 Student Result All rights reserved." 
+
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
