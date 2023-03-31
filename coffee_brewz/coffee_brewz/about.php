2f98b2ad6bfa952110e7f49ec974c6ce6cb013b2
asfg04 <129507434+asfg04@users.noreply.github.com>
2023-03-31 20:49:56 +0800

    Add coffee brewz website files (#7)

---
+++ coffee_brewz/coffee_brewz/about.php
@@ -0,0 +1,128 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="reviews">
+
+   <h1 class="title">clients reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/april.jpg" alt="">
+         <p>Best Coffee ever!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Pril</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/genesis.jpg" alt="">
+         <p>Good service,fast delivery!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Genesis</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/rollaine.jpg" alt="">
+         <p>Nice service!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Rollaine</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/ivy.jpg" alt="">
+         <p>Uulit at uulit ka sa sarap!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Ivy</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/diane.jpg" alt="">
+         <p>Good service!!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Diane</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <p>Did'nt expect the coffee, its delicious.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Myra</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
